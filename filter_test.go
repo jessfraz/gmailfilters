24ac15e61057ef5a71838c96695b3cd0c1ed7c14
Jess Frazelle <acidburn@github.com>
2018-12-29 03:14:54 -0500

    add tests
    Signed-off-by: Jess Frazelle <acidburn@github.com>

--- filter_test.go
+++ filter_test.go
@@ -19,11 +19,11 @@ func TestFilterToGmailFilters(t *testing.T) {
 				Label:             "Mailing Lists/coreos-dev",
 				ArchiveUnlessToMe: true,
 			},
-
 			expected: []gmail.Filter{
 				{
 					Action: &gmail.FilterAction{
-						AddLabelIds: []string{"1"},
+						AddLabelIds:    []string{"1"},
+						RemoveLabelIds: []string{},
 					},
 					Criteria: &gmail.FilterCriteria{
 						Query: "list:coreos-dev@googlegroups.com",
@@ -42,10 +42,75 @@ func TestFilterToGmailFilters(t *testing.T) {
 				},
 			},
 		},
+		"archive unless to me with OR": {
+			orig: filter{
+				QueryOr:           []string{"list:xdg-app@lists.freedesktop.org", "list:flatpak@lists.freedesktop.org"},
+				Label:             "Mailing Lists/xdg-apps",
+				ArchiveUnlessToMe: true,
+			},
+			expected: []gmail.Filter{
+				{
+					Action: &gmail.FilterAction{
+						AddLabelIds:    []string{"2"},
+						RemoveLabelIds: []string{},
+					},
+					Criteria: &gmail.FilterCriteria{
+						Query: "list:xdg-app@lists.freedesktop.org OR list:flatpak@lists.freedesktop.org",
+						To:    "me",
+					},
+				},
+				{
+					Action: &gmail.FilterAction{
+						AddLabelIds:    []string{"2"},
+						RemoveLabelIds: []string{"INBOX"},
+					},
+					Criteria: &gmail.FilterCriteria{
+						NegatedQuery: "to:me",
+						Query:        "list:xdg-app@lists.freedesktop.org OR list:flatpak@lists.freedesktop.org",
+					},
+				},
+			},
+		},
+		"delete": {
+			orig: filter{
+				QueryOr: []string{"to:plans@tripit.com", "to:receipts@expensify.com"},
+				Delete:  true,
+			},
+			expected: []gmail.Filter{
+				{
+					Action: &gmail.FilterAction{
+						AddLabelIds:    []string{"TRASH"},
+						RemoveLabelIds: []string{},
+					},
+					Criteria: &gmail.FilterCriteria{
+						Query: "to:plans@tripit.com OR to:receipts@expensify.com",
+					},
+				},
+			},
+		},
+		"archive and read": {
+			orig: filter{
+				QueryOr: []string{"to:plans@tripit.com", "to:receipts@expensify.com"},
+				Archive: true,
+				Read:    true,
+			},
+			expected: []gmail.Filter{
+				{
+					Action: &gmail.FilterAction{
+						AddLabelIds:    []string{},
+						RemoveLabelIds: []string{"INBOX", "UNREAD"},
+					},
+					Criteria: &gmail.FilterCriteria{
+						Query: "to:plans@tripit.com OR to:receipts@expensify.com",
+					},
+				},
+			},
+		},
 	}
 
 	labels := &labelMap{
 		strings.ToLower("Mailing Lists/coreos-dev"): "1",
+		strings.ToLower("Mailing Lists/xdg-apps"):   "2",
 	}
 
 	for name, tc := range testCases {
